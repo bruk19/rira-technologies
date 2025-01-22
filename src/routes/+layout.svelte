f2605802080df483ed01e9bb369779e8aaa77b95
bruk19 <brukteshome21@gmail.com>
2025-01-22 17:39:26 +0300

    init: setup enviroment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import '../app.css';
+import '../app.css';
 </script>
-<div class="h-[60px] w-full flex justify-center items-center fixed top-1 ">
-<div class="w-[60%] rounded-[20px] bg-red-200 mt-4">
-<div class="flex justify-between px-10 py-4 ">
-<h2>Rira-Technologies</h2>
-<div>
-<ul class="flex gap-10">
-<li>about</li>
-<li>about</li>
-<li>about</li>
-<li>about</li></ul>
-</div>
-</div>
-</div>
+<div class="h-[60px] w-full flex justify-center items-center fixed top-1 z-10 ">
+    <div class="w-[60%] rounded-[20px] bg-red-200 mt-4">
+        <div class="flex justify-between px-10 py-4 ">
+            <h2>Rira-Technologies</h2>
+            <div>
+                <ul class="flex gap-10">
+                    <li>about</li>
+                    <li>about</li>
+                    <li>about</li>
+                    <li>about</li></ul>
+            </div>
+        </div>
+    </div>
 </div>
 <slot />
 <h2>footer</h2>
